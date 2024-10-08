edc432b0fb5af770c889d6323ce1a8fc6c4159d3
BarnabasToth10 <tttbarna@gmail.com>
2024-10-08 13:05:53 +0200

    kész

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,25 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+    async function f1 (lat, long, timeStr) {
+    const querryStr = APIAdress + `latitute=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    console.log(querryStr);
+    const rArr = await  fetch(querryStr).then(data => data.json());
+    console.log(rArr);
+    let sum = 0;
+    rArr.hourly.temperature_2m.forEach(element => {
+        sum+= element
+    });
+    console.log(Math.floor(sum / 24));
+
+    
+    
+}
+//"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+module.exports = f1;
+
+f1(52,52, 13,41, 2024-10-3);
 }
 
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -1,6 +1,12 @@
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    //TODO
+    const querryStr = APIAdress + `latitute=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    console.log(querryStr);
+    const rArr = await  fetch(querryStr).then(data => data.json());
+
+    return rArr.hourly.temperature_2m.at(hour);
 }
+
+
 module.exports = f2;
