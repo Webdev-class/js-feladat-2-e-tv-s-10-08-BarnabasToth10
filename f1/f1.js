a38834a48410c6c2353e226cbcc1d0caac658b06
BarnabasToth10 <tttbarna@gmail.com>
2024-10-08 13:22:51 +0200

    kész(siker)

--- f1/f1.js
+++ f1/f1.js
@@ -1,17 +1,17 @@
 
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const locations = require('../locations.json')
 async function f1 (lat, long, timeStr) {
-    const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-    async function f1 (lat, long, timeStr) {
-    const querryStr = APIAdress + `latitute=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    const querryStr = APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
     console.log(querryStr);
-    const rArr = await  fetch(querryStr).then(data => data.json());
+    const rArr = await fetch(querryStr).then(data => data.json());
     console.log(rArr);
     let sum = 0;
+    console.log(rArr);
     rArr.hourly.temperature_2m.forEach(element => {
         sum+= element
     });
-    console.log(Math.floor(sum / 24));
+    return Math.floor(sum / 24);
 
     
     
@@ -19,7 +19,4 @@ async function f1 (lat, long, timeStr) {
 //"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
 module.exports = f1;
 
-f1(52,52, 13,41, 2024-10-3);
-}
-
-module.exports = f1;
+f1(locations.Budapest.latitude, locations.Budapest.longitude, "2024-10-03");
--- f2/f2.js
+++ f2/f2.js
@@ -1,9 +1,10 @@
 const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
 
 const f2 = async (lat, long, timeStr, hour) => {
-    const querryStr = APIAdress + `latitute=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
+    const querryStr = APIAdress + `latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
     console.log(querryStr);
-    const rArr = await  fetch(querryStr).then(data => data.json());
+    const rArr = await fetch(querryStr).then(data => data.json());
+
 
     return rArr.hourly.temperature_2m.at(hour);
 }
